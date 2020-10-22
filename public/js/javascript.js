238901e7be4a43218cfda6e13bda2041c763a75b
Mike Fallesen <mfallesen2010@gmail.com>
2020-10-22 10:40:07 -0700

    fix merge conflict

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,1 +1,12 @@
+$(document).ready(function() {
 
+    $(document).on("click", "#login", function() {
+        $("#login-modal").addClass("is-active");
+        console.log("Login clicked")
+    })
+
+    $(document).on("click", "#signup", function() {
+        $("#signup-modal").addClass("is-active");
+        console.log("Sign-up clicked")
+    })
+})
