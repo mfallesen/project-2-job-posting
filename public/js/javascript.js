cd2e5b17ceb0577ba560974f6cb4fee85b20f1af
Mike Fallesen <mfallesen2010@gmail.com>
2020-10-22 10:30:05 -0700

    jobpost.handlebars created

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,12 +1,1 @@
-$(document).ready(function() {
 
-    $(document).on("click", "#login", function() {
-        $("#login-modal").addClass("is-active");
-        console.log("Login clicked")
-    })
-
-    $(document).on("click", "#signup", function() {
-        $("#signup-modal").addClass("is-active");
-        console.log("Sign-up clicked")
-    })
-})
