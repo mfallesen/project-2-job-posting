3f3b549a8699628e89362d3226707dd139f607f6
Mike Fallesen <mfallesen2010@gmail.com>
2020-10-22 23:45:20 -0700

    work in nodemailer and start on event handlers to fire the email

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,21 +1,67 @@
-$(document).ready(function() {
+// Nodemailer Implementation
+const nodemailer = require('nodemailer'); /* this wont work browser side. Ask TA for assistance tomorrow? browserify? */
 
-    $(document).on("click", "#viewjobs", function() {
-        location.href="/job/listings";
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'nextstep123@gmail.com',
+        pass: 'MYtest123!@#'
+    }
+})
+// ++++++++++++++
+// Place inside event listener for submit
+// ++++++++++++++
+
+// +++++++++++++
+
+
+
+
+
+
+
+
+$(document).ready(function () {
+
+    $(document).on("click", "#viewjobs", function () {
+        location.href = "/job/listings";
     })
 
-    $(document).on("click", "#login", function() {
+    $(document).on("click", "#login", function () {
         $("#login-modal").addClass("is-active");
     })
 
-    $(document).on("click", "#signup", function() {
+    $(document).on("click", "#signup", function () {
         $("#signup-modal").addClass("is-active");
     })
 
-    $(".delete").on("click", function() {
+    $(".delete").on("click", function () {
         $(".modal").removeClass("is-active")
     })
 
-   
-    
+    $("#contact-close").on("click", function () {
+        $(".modal").removeClass("is-active")
+    })
+    // +++++++++++++++
+    // Work In Progress
+    $("#contact-send").addEventListener("click", function (event) {
+        event.preventDefault();
+
+        const mailOptions = {
+            from: `${"#contact-email".value}`,
+            to: 'random manager'/* manager email inserted here */,
+            subject: `${"#contact-name".value} is interested in a job on NextStep`,
+            text: `${"#contact-message".value}`
+        };
+        
+        transporter.sendMail(mailOptions, function (err, info) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('message sent: ' + info.response);
+            }
+        });
+    })
+    // +++++++++++++++
+
 })
